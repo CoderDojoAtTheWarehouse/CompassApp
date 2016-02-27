d9e01e9d9a8d900bec28df5f12b1640a7604aa43
Brian Matthews <brian@btmatthews.com>
2016-02-27 22:19:31 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+http://developer.android.com/guide/topics/sensors/sensors_overview.html
+
+http://www.techrepublic.com/article/pro-tip-create-your-own-magnetic-compass-using-androids-internal-sensors/
---
+++ app/src/main/java/com/btmatthews/compassapp/MainActivity.java
@@ -0,0 +1,91 @@
+package com.btmatthews.compassapp;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+
+    private Sensor compass;
+
+    private Sensor accelerometer;
+
+    private ImageView pointer;
+
+    private float[] lastAccelerometer = new float[3];
+
+    private float[] lastMagnetometer = new float[3];
+
+    private boolean lastAccelerometerSet = false;
+
+    private boolean lastMagnetometerSet = false;
+
+    private float currentDegree = 0f;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pointer = (ImageView)findViewById(R.id.pointer);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        compass = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    public final void onAccuracyChanged(final Sensor sensor, final int accuracy) {
+    }
+
+    @Override
+    public final void onSensorChanged(final SensorEvent event) {
+        if (event.sensor == compass) {
+            System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.length);
+            lastMagnetometerSet = true;
+        } else if (event.sensor == accelerometer) {
+            System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.length);
+            lastAccelerometerSet = true;
+        }
+
+        if (lastAccelerometerSet && lastMagnetometerSet) {
+            final float[] rotation = new float[9];
+            final float[] orientation = new float[3];
+            SensorManager.getRotationMatrix(rotation, null, lastAccelerometer, lastMagnetometer);
+            SensorManager.getOrientation(rotation, orientation);
+            float azimuthInRadians = orientation[0];
+            float azimuthInDegrees = (float)(Math.toDegrees(azimuthInRadians)+360)%360;
+            final RotateAnimation ra = new RotateAnimation(
+                    currentDegree,
+                    -azimuthInDegrees,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f);
+            ra.setDuration(250);
+            ra.setFillAfter(true);
+            pointer.startAnimation(ra);
+            currentDegree = -azimuthInDegrees;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, compass, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}

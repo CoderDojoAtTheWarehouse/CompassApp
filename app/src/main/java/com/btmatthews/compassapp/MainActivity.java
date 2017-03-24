395cb03cfe53975c764b81a0b43a77dffe5a3f5d
Brian Matthews <brian.matthews@synchronoss.com>
2017-03-24 08:38:10 +0000

    Updated work in progress

--- app/src/main/java/com/btmatthews/compassapp/MainActivity.java
+++ app/src/main/java/com/btmatthews/compassapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
+f
 
     private Sensor compass;
 
